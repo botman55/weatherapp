83ca52fec29fb6ec9b6762faf90941a4747b815f
P00P.head <tullu20top@gmail.com>
2023-03-10 06:47:44 +0530

    version 1.9

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,10 @@ const apiKey = 'a29aa41001ceafbdc70829de0a6fc613';
 const apiKey2 = 'd73f43cd2bc0437f899174523230703';
 let data;
 document.cookie = "user=bot;expires=Tue, 15 Mar 2023 00:00:00 UTC"
-// if(document.cookie=='')
-//     document.cookie="loc=;expires=Tue, 15 Feb 2023 00:00:00 UTC"
+
 
 const getweather = async (city) => {
-    // const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metrics`;
-    // const url=`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&units=metrics`;
+    
     if (getCityfromCookie() == city) {
         document.getElementById('star').classList.remove('fa-star-o');
         document.getElementById('star').classList.add('fa-star');
@@ -30,10 +28,7 @@ const getweather = async (city) => {
     fivedays(data2);
 }
 
-// let cityname;
-// cityname=prompt("Enter your current city");
-// document.cookie="user=kislay;expires=Tue, 15 Feb 2023 00:00:00 UTC";
-// document.cookie=`loc=${cityname};expires=Tue, 15 Feb 2023 00:00:00 UTC`;
+
 function getCityfromCookie() {
     let cont = decodeURIComponent(document.cookie);
     let dc = cont.split(";");
@@ -41,7 +36,7 @@ function getCityfromCookie() {
     for (let i = 0; i < dc.length; i++) {
         let word = dc[i];
         if (word.substring(0, 5) == " loc=") {
-            //console.log(`${word.slice(4,)}`);
+           
             cn = `${word.slice(5,)}`;
         }
     } 
@@ -55,7 +50,7 @@ function begin() {
         getweather(cityname);
     }
 }
-// getweather(cn);
+
 begin();
 function setvals(data,data2) {
     var format = setdate(data2);
@@ -79,7 +74,7 @@ function setdate(data) {
 }
 function searchweatherdata() {
     let city = document.getElementById('search-city').value;
-    // console.log(city);
+    
     getweather(city);
     document.getElementById('search-city').value = '';
 }
@@ -96,14 +91,14 @@ function setfav() {
     let status;
     if (element.classList == 'fa fa-star-o') {
         status = 1;
-        // console.log(status);
+        
         element.classList.remove('fa-star-o');
         element.classList.add('fa-star');
         document.cookie = `loc=${data.name};expires=Tue, 15 Mar 2023 00:00:00 UTC`;
     }
     else {
         status = 0;
-        // console.log(status);
+        
         element.classList.remove('fa-star');
         console.log();
         element.classList.add('fa-star-o');
@@ -116,47 +111,6 @@ function setfav() {
 function fivedays(data) {
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     
-   /* document.getElementById('today_min').innerText = `${data.forecast.forecastday[0].day.mintemp_c} °C`;
-    document.getElementById('today_max').innerText = `${data.forecast.forecastday[0].day.maxtemp_c} °C`;
-    document.getElementById('today_sunrise').innerText = `${data.forecast.forecastday[0].astro.sunrise}`;
-    document.getElementById('today_sunset').innerText = `${data.forecast.forecastday[0].astro.sunset}`;
-    document.getElementById('todayIcon').innerHTML=`<img src="https:${data.forecast.forecastday[0].day.condition.icon}">`;
-    document.getElementById('icon').innerHTML=`<img src="https:${data.forecast.forecastday[0].day.condition.icon}">`;
-
-    document.getElementById('tempMin1').innerText = `${data.forecast.forecastday[1].day.mintemp_c} °C`;
-    document.getElementById('tempMax1').innerText = `${data.forecast.forecastday[1].day.maxtemp_c} °C`;
-    document.getElementById('wicon1').innerHTML=`<img src="https:${data.forecast.forecastday[1].day.condition.icon}">`;
-
-
-    document.getElementById('tempMin2').innerText = `${data.forecast.forecastday[2].day.mintemp_c} °C`;
-    document.getElementById('tempMax2').innerText = `${data.forecast.forecastday[2].day.maxtemp_c} °C`;
-    document.getElementById('wicon2').innerHTML=`<img src="https:${data.forecast.forecastday[2].day.condition.icon}">`;
-    let a=new Date(`${data.forecast.forecastday[2].date}`);
-    document.getElementById('day2').innerText = `${weekday[a.getDay()]}`;
-
-    document.getElementById('tempMin3').innerText = `${data.forecast.forecastday[3].day.mintemp_c} °C`;
-    document.getElementById('tempMax3').innerText = `${data.forecast.forecastday[3].day.maxtemp_c} °C`;
-    document.getElementById('wicon3').innerHTML=`<img src="https:${data.forecast.forecastday[3].day.condition.icon}">`;
-    let b=new Date(`${data.forecast.forecastday[3].date}`);
-    document.getElementById('day3').innerText = `${weekday[b.getDay()]}`;
-
-    document.getElementById('tempMin4').innerText = `${data.forecast.forecastday[4].day.mintemp_c} °C`;
-    document.getElementById('tempMax4').innerText = `${data.forecast.forecastday[4].day.maxtemp_c} °C`;
-    document.getElementById('wicon4').innerHTML=`<img src="https:${data.forecast.forecastday[4].day.condition.icon}">`;
-    let c=new Date(`${data.forecast.forecastday[4].date}`);
-    document.getElementById('day4').innerText = `${weekday[c.getDay()]}`;
-
-    document.getElementById('tempMin5').innerText = `${data.forecast.forecastday[5].day.mintemp_c} °C`;
-    document.getElementById('tempMax5').innerText = `${data.forecast.forecastday[5].day.maxtemp_c} °C`;
-    document.getElementById('wicon5').innerHTML=`<img src="https:${data.forecast.forecastday[5].day.condition.icon}">`;
-    let d=new Date(`${data.forecast.forecastday[5].date}`);
-    document.getElementById('day5').innerText = `${weekday[d.getDay()]}`;
-
-    document.getElementById('tempMin6').innerText = `${data.forecast.forecastday[6].day.mintemp_c} °C`;
-    document.getElementById('tempMax6').innerText = `${data.forecast.forecastday[6].day.maxtemp_c} °C`;
-    document.getElementById('wicon6').innerHTML=`<img src="https:${data.forecast.forecastday[6].day.condition.icon}">`;
-    let e=new Date(`${data.forecast.forecastday[6].date}`);
-    document.getElementById('day6').innerText = `${weekday[e.getDay()]}`;*/
 
     document.getElementById('today_min').innerText = `${data.forecast.forecastday[0].day.mintemp_c}°C`;
     document.getElementById('today_max').innerText = `${data.forecast.forecastday[0].day.maxtemp_c}°C`;
--- scripts/ui.js
+++ scripts/ui.js
@@ -10,36 +10,7 @@ menuBtn.addEventListener('click',()=>{
 closeBtn.addEventListener('click',()=>{
     sideMenu.style.display='none';
 })
-/*themeToggle.addEventListener('click',()=>
-{
-    console.log(`status now ${status}`);
-    if(status==1){
-        console.log("convertung dark mode");
-        document.body.classList.toggle('dark-theme-variables');
-        document.getElementById('wind').style.filter="invert(1)";
-        document.getElementById('humid').style.filter="invert(1)";
-        document.getElementById('uv').style.filter="invert(1)";
-        document.getElementById('press').style.filter="invert(1)";
-        document.getElementById('visi').style.filter="invert(1)";
-        document.getElementById('preci').style.filter="invert(1)";
-        document.querySelector('.limode').classList.remove('active');
-        document.querySelector('.drmode').classList.add('active');
-        status=0;
-    }
-    else{
-        console.log("back to light hss");
-        document.body.classList.toggle('light-theme-variables');
-        document.getElementById('wind').style.filter="invert(0)";
-        document.getElementById('humid').style.filter="invert(0)";
-        document.getElementById('uv').style.filter="invert(0)";
-        document.getElementById('press').style.filter="invert(0)";
-        document.getElementById('visi').style.filter="invert(0)";
-        document.getElementById('preci').style.filter="invert(0)";
-        document.querySelector('.drmode').classList.remove('active');
-        document.querySelector('.limode').classList.add('active');
-        status=1;
-    }
-})*/
+
 
 themeToggle.addEventListener('click',()=>
 {
